fb082c948214c3c305974010e44784bf9bbb8aae
alexandra002dev <alexandramarkess@gmail.com>
2025-06-09 15:51:46 -0300

    prisma

--- build/models/tasks.js
+++ build/models/tasks.js
@@ -10,8 +10,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateTask = exports.deleteTask = exports.createTask = exports.getTasks = void 0;
-const prisma_1 = require("../generated/prisma");
-const prisma = new prisma_1.PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 // GET - Listar todas as tarefas do usuário
 const getTasks = (userId) => __awaiter(void 0, void 0, void 0, function* () {
     try {
