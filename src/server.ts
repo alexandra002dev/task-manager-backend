2bb9d7d188aee0f7354d392a2fc174237ce45c6b
alexandra002dev <alexandramarkess@gmail.com>
2025-06-10 09:45:21 -0300

    fix: corrigir configuração package.json

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-import cors from "cors";
-const server = express();
 
+const server = express();
+const cors = require("cors");
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
